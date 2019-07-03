f5defee22958c9e82aace7dfd4eac67e293a78b9
adanb <adanb@Gordon-PC-2>
2019-07-03 16:42:49 +0800

    Basic Encryptor. log4j

--- src/main/java/com/gordon/app/Encryptor.java
+++ src/main/java/com/gordon/app/Encryptor.java
@@ -1,29 +1,68 @@
 package com.gordon.app;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Encrypt file application.
+ * 
+ * Need to specify random keys. 
+ * Input folders, output folders.
+ * @author adanb
+ *
+ */
 public class Encryptor {
 	
-	public static void main(String[]args)
+	static Logger logger = Logger.getLogger(Encryptor.class);
+	public static void main(String[]args) throws NoSuchAlgorithmException, NoSuchPaddingException, IOException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
 	{
+		
+		logger.info("Getting cipher..");
+		KeyGenerator kgen = KeyGenerator.getInstance("AES");
+		kgen.init(128,new SecureRandom("".getBytes())); //here need to update.
+		SecretKey secretKey = kgen.generateKey();
+		byte[]enCodeFormat = secretKey.getEncoded();
+		SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		
 		File folder = new File("...");
-		
-		
-		
 		if(folder.exists())
 		{
-			System.out.println("Encrypting files..");
+			logger.info("Encrypting files..");
 			
 			for(File file:folder.listFiles())
 			{
-				System.out.println("Encrypting file： " + file.getName());
+				if(file.isDirectory())
+				{
+					
+				}
+				logger.info("Encrypting file： " + file.getName());
+				byte[] byteContent = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+				byte[] result = cipher.doFinal(byteContent);
 				
 			}
 		}
 		
 		else
 		{
-			System.out.println("Folder not exist.");
+			logger.error("Folder not exist.");
 		}
 	}
 
